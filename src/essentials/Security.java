79b982a344b72d764ead3e3a2989f9d60d28df31
leethaxor <felix.beutter@outlook.com>
2016-11-20 20:45:50 +0100

    Added hash (sha-256) method to 'Security' class

--- src/essentials/Security.java
+++ src/essentials/Security.java
@@ -1,5 +1,32 @@
 package essentials;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class Security {
 
+	/**
+	 * Returns the SHA-256 hash of a given string
+	 * 
+	 * @param string
+	 *            the String to be hashed
+	 * @return the hash of the String
+	 */
+	public static String hash(String string) {
+
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA-256");
+			md.update(string.getBytes());
+			byte[] b = md.digest();
+			StringBuffer sb = new StringBuffer();
+
+			for (int i = 0; i < b.length; i++) {
+				sb.append(Integer.toString((b[i] & 0xff) + 0x100, 16).substring(1));
+			}
+
+			return sb.toString();
+		} catch (NoSuchAlgorithmException e) {
+			return null;
+		}
+	}
 }
